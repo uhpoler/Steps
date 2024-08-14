4f2b4a9d5345494bc16c886195d342efad7267e8
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-14 16:50:32 +0300

    Added emoji and children prop

--- src/App.js
+++ src/App.js
@@ -35,18 +35,13 @@ export default function App() {
           </p>
 
           <div className="buttons">
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
-              onClick={handlePrevious}
-              text="Previouse"
-            />
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
-              onClick={handleNext}
-              text="Next"
-            />
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              <span>👈</span>Previouse
+            </Button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+              Next
+              <span>👉</span>
+            </Button>
           </div>
         </div>
       )}
@@ -54,13 +49,13 @@ export default function App() {
   );
 }
 
-function Button({ textColor, bgColor, onClick, text }) {
+function Button({ textColor, bgColor, onClick, children }) {
   return (
     <button
       style={{ backgroundColor: bgColor, color: textColor }}
       onClick={onClick}
     >
-      {text}
+      {children}
     </button>
   );
 }
